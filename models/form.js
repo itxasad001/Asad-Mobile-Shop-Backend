5761094991691e56e6b8de829d603c5d6ff7e926
Zeeshan Ali Zafar <zeeshanalizafar53@gmail.com>
2026-06-08 13:49:31 +0500

    new

--- models/form.js
+++ models/form.js
@@ -12,7 +12,7 @@ const FormSchema = new mongoose.Schema({
 
 },{timestamps:true})
 
-const FormModel = mongoose.model('ShopData',FormSchema)
+const FormModel = mongoose.model('expense',FormSchema)
 
 
 export default FormModel
